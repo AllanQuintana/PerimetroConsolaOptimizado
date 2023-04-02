a8119088975f5c6a6852f91d3cfdcca603d838f9
ALLAN SEBASTIAN QUINTANA ABARCA <allan.quintana@gmail.com>
2023-04-02 17:02:31 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace ConPerimetroOptimizado
+{
+    class Program
+    {
+        static void Main()
+        {
+            Write("Ingrese el alto del rectangulo: ");
+            double.TryParse(ReadLine(), out double alto);
+
+            Write("Ingrese el ancho del rectangulo: ");
+            double.TryParse(ReadLine(), out double ancho);
+
+            double perimetro = 2 * (alto + ancho);
+            WriteLine($"El perimetro es {perimetro}");
+
+            ReadKey();
+        }
+    }
+}
